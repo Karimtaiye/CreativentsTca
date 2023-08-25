c776142a333b7ba0cf8640bdc9502dde999662ac
Karimtaiye <olayinkahassan117@gmail.com>
2023-08-26 00:20:11 +0100

    oya

--- src/Components/ChangePassword/ChangePassword.jsx
+++ src/Components/ChangePassword/ChangePassword.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './ChangePassword.css'
 import './ChangePasswordMedia.css'
 import { useSelector } from 'react-redux'
@@ -8,15 +8,21 @@ import {AiFillHome} from 'react-icons/ai'
 import {MdCreateNewFolder} from 'react-icons/md'
 import {BsFillCheckSquareFill} from 'react-icons/bs'
 import LogoC from "../../assets/LogoC.png"
+import { GiConfirmed } from 'react-icons/gi'
+import { BiSolidError } from 'react-icons/bi'
 function ChangePassword() {
   const nav = useNavigate()
   const { id } = useParams()
+  
   const userSignUpData = useSelector(state=>state.events.userRes)
   const userOnLoggedIn = useSelector(state=>state.events.user)
   const [currentPassword, setCurrentPassword] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [succMsg, setSuccMsg] = useState('');
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false)
+  const [visible, setVisible] = useState(false)
   
   const token = userOnLoggedIn.token
   const config = {
@@ -27,22 +33,65 @@ function ChangePassword() {
 
 
   const url = `https://creativents-on-boarding.onrender.com/api/changepassword/${id}`
+  useEffect(()=>{
+    if(currentPassword === "")
+  {
+    setLoading(true)
+  }
+    else if(password === "")
+  {
+    setLoading(true)
+  }
+    else if(confirmPassword === "")
+  {
+    setLoading(true)
+  }
+  else{
+    setLoading(false)
+  }
+  },[currentPassword, confirmPassword, password])
   const ChangePassword = () => {
+    setLoading(true)
     if (password !== confirmPassword) {
       alert('New password and confirm password do not match.');
+      setLoading(false)
     }
     else{
       axios.put(url, {password} , config)
       .then(res=>{
-        console.log(res)
-        nav('/login')
-      })
-      .catch(err=>{
-        console.log(err)
-      })
-    }
-  };
+        setLoading(false)
+        console.log(res.data.data)     
+*        setVisible(true);
 
+          setTimeout(() => {
+              setVisible(false);
+              nav('/homepage'); 
+            }, 5000)
+        if (res){
+            console.log("response sent")
+            setSuccMsg("Password Updated Successfully")
+        }else{
+            console.log('Cannot Update Profile')
+        }
+    })
+    .catch(err=>{
+        console.log(err);
+        setLoading(false)
+        setError(true)
+        setVisible(true);
+        setTimeout(() => {
+        setVisible(false);
+          }, 3000)
+        if(err.message === "Network Error"){
+          setSuccMsg("Please check your Internet Connection")
+        }
+        else{ 
+          setSuccMsg("Error Creating Event")
+          }
+    })
+
+  }
+}
   return (
     <div className='User_ChangePassword'>
       <div className='mypasswordHolder'>
@@ -61,11 +110,49 @@ function ChangePassword() {
         <label>Confirm Password:</label>
         <input type="text" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)} />
       </div>
-      <button onClick={ChangePassword}>Change Password</button>
+      <button style={{background:loading?"#815907":null}} disabled={loading} onClick={ChangePassword}>Change Password</button>
       </div>
     </div>
 
+    {
+            visible ?
+            <div className="Update_PopUpMsg">
+            <h2>{succMsg}</h2>
+           {
+             error?<BiSolidError style={{fontSize:"100px", color:"red"}}/> :         
+             <GiConfirmed style={{fontSize:"100px", color:"green"}}/> 
+           }
+            {/* <button className="Canceled_Btn" onClick={()=>nav('/homepage')}>Go Back</button> */}
+        </div>:null  
+          }
+
     <div className="directiontodifferentpage">
+            <div className="Homedirection">
+                <AiFillHome onClick={()=>nav('/homepage')} className="directionmain"/>
+                <h5>Home</h5>
+            </div>
+
+            <div className="Homedirection">
+                <MdCreateNewFolder onClick={()=>nav('/upload')} className="directionmain"/>
+                <h5>Create</h5>
+            </div>
+            <div className="Homedirection">
+                <BsFillCheckSquareFill onClick={()=>nav(`/api/getUserWithLinks/${id}`)} className="directionmain"/>
+                <h5>My events</h5>
+            </div>
+          </div>
+          {
+            visible ?
+            <div className="Update_PopUpMsg">
+            <h2>{succMsg}</h2>
+           {
+             error?<BiSolidError style={{fontSize:"100px", color:"red"}}/> :         
+             <GiConfirmed style={{fontSize:"100px", color:"green"}}/> 
+           }
+            {/* <button className="Canceled_Btn" onClick={()=>nav('/homepage')}>Go Back</button> */}
+        </div>:null  
+          }
+           <div className="directiontodifferentpage">
             <div className="Homedirection">
                 <AiFillHome onClick={()=>nav('/homepage')} className="directionmain"/>
                 <h5>Home</h5>
